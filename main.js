195fb716c2e884d7d11a6d2caab0c0101f487cbb
shrubfromtomorrow <orionjandrews@gmail.com>
2022-09-20 10:35:40 -0700

    Ended my stint with paperjs due to it's unusability. Created light object in order to store each light as something other than a dumb pixel

--- main.js
+++ main.js
@@ -3,28 +3,57 @@ const canvas = document.querySelector("#main");
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-console.log(window.innerWidth);
-console.log(window.innerWidth - 20);
+
+
+function Light(id, x, y, radius, color) {
+  this.id = id;
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
+}
+
+
+let lightArray = [];
+
 
 let j = 0;
 
 function animate() {
-  if (j > 50) {
+  if (j > 0) {
     clearInterval(interval)
+
+    rand = Math.floor(Math.random() * 249);
+    console.log(rand);
+
+    ctx.beginPath();
+    ctx.fillStyle = "pink";
+    x = lightArray[rand][1];
+    y = lightArray[rand][2];
+    ctx.arc(x, y, 20, 0, Math.PI*2);
+    ctx.fill();
   }
 
   else {
-    for (let i = 0; i<20; i++){
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    for (let i = 0; i<250; i++){
       ctx.beginPath();
       ctx.fillStyle = "pink";
-      randX = Math.floor(10 + (Math.random() * (window.innerWidth - 20)));
-      randY = Math.floor(10 + (Math.random() * (window.innerHeight - 20)));
-      ctx.arc(randX, randY, 10, 0, Math.PI*2);
+      randX = Math.floor(6 + (Math.random() * (window.innerWidth - 12)));
+      randY = Math.floor(6 + (Math.random() * (window.innerHeight - 12)));
+      radius = 3;
+      ctx.arc(randX, randY, radius, 0, Math.PI*2);
       ctx.fill();
+      lightArray.push(new Light(i, randX, randY, radius, ctx.fillStyle));
     }
     j++;
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   }
 }
 
-let interval = setInterval(animate, 10)
+function clear() {
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+}
+
+let interval = setInterval(animate, 100)
+
+
